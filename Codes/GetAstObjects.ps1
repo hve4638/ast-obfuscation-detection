a9acaae178d81d8fbf09bdcf4d624211fa11f53f
yeeun <irinap9270@gmail.com>
2024-10-07 14:31:54 +0900

    Second Commit

--- Codes/GetAstObjects.ps1
+++ Codes/GetAstObjects.ps1
@@ -1,28 +1,35 @@
-param (
+# 경로를 받아오는 매개변수
+param(
 	[String]$path
 )
 
+# Parser를 이용해 AST 추출
 $ast = [System.Management.Automation.Language.Parser]::ParseInput((Get-Content $path), [ref]$null, [ref]$null)
 
 $predicate = { $true }
 
 $recurse = $true
 
+# 객체 타입 추출
 $type = @{
   Name = 'Type'
   Expression = { $_.GetType().Name }
 }
 
+# 코드 위치 추출
 $position = @{
   Name = 'Position'
   Expression = { '{0,3}-{1,-3}' -f  $_.Extent.StartOffset, $_.Extent.EndOffset, $_.Extent.Text }
 }
 
+# 코드 추출
 $text = @{
   Name = 'Code'
   Expression = { $_.Extent.Text }
 }
 
-$astnodeTypes = $ast.FindAll($predicate, $recurse) | ForEach-Object {$_.GetType().Name}
+# AST 노드 위에서부터 탐색
+$astObjects = $ast.FindAll($predicate, $recurse)
 
-return $astnodeTypes
+# AST 타입, 코드 위치, 코드 출력
+$astObjects | Select-Object -Property $position, $type, $text
--- Codes/GetNGrams.ps1
+++ Codes/GetNGrams.ps1
@@ -1,15 +1,22 @@
+# AST 타입 리스트, N을 받아오는 매개변수
 param (
 	[string[]]$InputArr,
 	[int]$N
 )
+
+# AST 타입 리스트 확인
 if (-not $InputArr) {
 	Write-Error "InputArr is empty."
 	return @()
 }
+
+# 결과를 저장할 빈 배열
 $nGrams = @()
 
+# InputArr로부터 N-Gram 추출
 for ($i = 0; $i -le $InputArr.Length - $N; $i++) {
 	$nGram = $InputArr[$i..($i + $N - 1)] -join ','
 	$nGrams += "($nGram)"
 }
+
 return $nGrams
