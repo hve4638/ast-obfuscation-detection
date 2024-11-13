418e4266f8cb79381b822d855c78ba1aa31e50c1
Hve <hve4638@gmail.com>
2024-11-13 13:35:46 +0900

    add

--- VirusTotal/app.py
+++ VirusTotal/app.py
@@ -12,24 +12,30 @@ dataset_paths = [
 output_path = '../../vt_result/'
 
 pass_count = 0
+error_count = 0
 skip_count = 0
 for dataset_path in dataset_paths:
     for dirpath, _, filenames in os.walk(dataset_path):
         for filename in filenames:
             target = os.path.join(dirpath, filename)
             _, lastpath = os.path.split(dirpath)
-            output = os.path.join(output_path, lastpath, filename + '.json')
+            outputdir = os.path.join(output_path, lastpath)
+            output = os.path.join(outputdir, filename + '.json')
+            os.makedirs(outputdir, exist_ok=True)
             
-            report = upload_vt(target, api_key)
-            if report is None:
-                skip_count += 1
-                print(f'skip : {target}')
+            if not os.path.isfile(output):
+                report = upload_vt(target, api_key)
+                if report is None:
+                    error_count += 1
+                    print(f'error : {target}')
+                else:
+                    pass_count += 1
+                    print(f'VT : {target}')
+                    with open(output, 'w') as f:
+                        json.dump(report, f, indent=4)
             else:
-                pass_count += 1
-                print(f'VT : {target}')
-                with open(output, 'w') as f:
-                    json.dump(report, f, indent=4)
-                break
-            
+                skip_count += 1
+                
 print(f'pass: {pass_count}')
 print(f'skip: {skip_count}')
+print(f'error: {error_count}')
